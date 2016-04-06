ea091cdbc7e76435e1ad157dc0307946f6192731
Justin Hoyt <justinhoyt24@gmail.com>
2016-04-06 22:00:34 +0000

    inner and outer semaphore framework

--- Car.java
+++ Car.java
@@ -7,10 +7,6 @@ public class Car {
     public Enum.Direction targetDirection;
     public int carID;
     public double arrivalTime;
-    // boolean northEastLock = false;
-    // boolean northWestLock = false;
-    // boolean southEastLock = false;
-    // boolean southWestLock = false;
     
     public Car(int carID, double arrivalTime, Enum.Direction originalDirection, Enum.Direction targetDirection){
         this.originalDirection = originalDirection;
@@ -27,22 +23,6 @@ public class Car {
     }
     
     public void arriveIntersection(Intersection intersection){
-    /*
-        When a car arrives at the intersection, if it sees a car (with the same original
-        direction) stopping in front, it should wait until the front car starts crossing (it doesn’t need to wait
-        until the front car finishes crossing). This is called head-of-line blocking. Otherwise, it can either
-        drive through, or turn left, or turn right (U-turn is not allowed). But it should first check the traffic
-        light in front.
-        • Green Light: If it attempts to drive through, make sure that no car from the opposite direction
-        is turing left, or any car is turning right to the same lane; if it attempts to turn left, make sure
-        that no car is driving through the intersection in the opposite direction, or any car is turning right
-        to the same lane; if it attempts to turn right, make sure that no car is driving to the same lane.
-        • Yellow Light: If it attempts to turn right, make sure that no car is driving to the same lane,
-        otherwise it should stop in front of the intersection.
-        • Red Light: If it attempts to drive through or turn left, it should wait for the Green light and then
-        follows the rule for Green Light; if it attempts to turn right, make sure that no car is driving to
-        the same lane.
-    */
         //if straight
         if(originalDirection.getNumber() == targetDirection.getNumber()){
             goStraight(intersection);
@@ -73,10 +53,6 @@ public class Car {
                " ->" + targetDirection.getName() + ")";
     }
     public void crossIntersection(){
-        /*
-            We assume that it takes a fixed time period TC to cross the intersection and print
-            out a debug message. You could use the Spin function to simulate the crossing.
-        */
         try {
             Thread.sleep(1000);                 //1000 milliseconds is one second.
         } catch(InterruptedException ex) {
@@ -85,11 +61,6 @@ public class Car {
         System.out.println(this.toString() + " crossing");
     }
     public void exitIntersection(Intersection intersection){
-        /*
-            It is called to indicate that the caller has finished crossing the intersection and
-            should take steps to let additional cars cross the intersection.
-        */
-        
         //if straight
         if(originalDirection.getNumber() == targetDirection.getNumber()){
             int semaphoreToGet1 = (this.originalDirection.getNumber() + 1) % 4;
@@ -120,30 +91,10 @@ public class Car {
         }
         
         System.out.println(this.toString() + " exiting");
-        
-        
-    /*    
-        if(car.northEastLock == true){
-            intersection.northEastLock.release();
-            car.northEastLock = false;
-        }
-        if(car.northWestLock == true){
-            intersection.northWestLock.release();
-            car.northWestLock = false;
-        }
-        if(car.southEastLock == true){
-            intersection.southEastLock.release();
-            car.southEastLock = false;
-        }
-        if(car.southWestLock == true){
-            intersection.southWestLock.release();
-            car.southWestLock = false;
-        }
-    */
     }
 
     
-    public void goStraight(Intersection intersection) {
+    public synchronized void goStraight(Intersection intersection) {
         int semaphoreToGet1 = (this.originalDirection.getNumber() + 1) % 4;
         int semaphoreToGet2 = (this.originalDirection.getNumber() + 2) % 4;
         Semaphore semaphore1 = intersection.semaphore.get(semaphoreToGet1);
@@ -160,19 +111,19 @@ public class Car {
             e.printStackTrace();
         }
     }
-    public void goRight(Intersection intersection) {
+    public synchronized void goRight(Intersection intersection) {
         //calculate semaphore
         int semaphoreToGet = (this.originalDirection.getNumber() + 2) % 4;
         
         //acquire calculated semaphore
-        Semaphore semaphore = intersection.semaphore.get(semaphoreToGet);
+        Semaphore innerSemaphore = intersection.innerSemaphore.get(semaphoreToGet);
         try {
-            semaphore.acquire();
+            innerSemaphore.acquire();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    public void goLeft(Intersection intersection) {
+    public synchronized void goLeft(Intersection intersection) {
         int semaphoreToGet1 = (this.originalDirection.getNumber() + 2) % 4;
         int semaphoreToGet2 = this.originalDirection.getNumber();
         Semaphore semaphore1 = intersection.semaphore.get(semaphoreToGet1);
--- Intersection.java
+++ Intersection.java
@@ -6,31 +6,60 @@ public class Intersection {
     public Stoplight south;
     public Stoplight east;
     public Stoplight west;
+    
     public Semaphore northEastLock;
     public Semaphore northWestLock;
     public Semaphore southEastLock;
     public Semaphore southWestLock;
-    public HashMap<Integer, Semaphore> semaphore;
+    
+    public Semaphore northEntrance;
+    public Semaphore eastEntrance;
+    public Semaphore southEntrance;
+    public Semaphore westEntrance;
+    
+    public Semaphore northExit;
+    public Semaphore eastExit;
+    public Semaphore southExit;
+    public Semaphore westExit;
+    
+    public HashMap<Integer, Semaphore> innerSemaphore;
+    public HashMap<Integer, Semaphore> outterSemaphore;
     
     public Intersection(){
         north = new Stoplight(Enum.Direction.NORTH, Enum.Color.GREEN);
         south = new Stoplight(Enum.Direction.SOUTH, Enum.Color.GREEN);
         east = new Stoplight(Enum.Direction.EAST, Enum.Color.RED);
         west = new Stoplight(Enum.Direction.WEST, Enum.Color.RED);
+        
         northEastLock = new Semaphore(1, true);
         northWestLock = new Semaphore(1, true);
         southEastLock = new Semaphore(1, true);
         southWestLock = new Semaphore(1, true);
         
-        semaphore = new HashMap<Integer, Semaphore>();
-        semaphore.put(0, northWestLock);
-        semaphore.put(1, northEastLock);
-        semaphore.put(2, southEastLock);
-        semaphore.put(3, southWestLock);
+        northEntrance = new Semaphore(1, true);
+        eastEntrance = new Semaphore(1, true);
+        southEntrance = new Semaphore(1, true);
+        westEntrance = new Semaphore(1, true);
         
-    }
-    
-    public Semaphore getSemaphore(int semaphoreNumberToGet){
-        return this.semaphore.get(semaphoreNumberToGet);
+        northExit = new Semaphore(1, true);
+        eastExit = new Semaphore(1, true);
+        southExit = new Semaphore(1, true);
+        westExit = new Semaphore(1, true);
+        
+        innerSemaphore = new HashMap<Integer, Semaphore>();
+        innerSemaphore.put(0, northWestLock);
+        innerSemaphore.put(1, northEastLock);
+        innerSemaphore.put(2, southEastLock);
+        innerSemaphore.put(3, southWestLock);
+        
+        outterSemaphore = new HashMap<Integer, Semaphore>();
+        outterSemaphore.put(0, southEntrance);
+        outterSemaphore.put(1, northExit);
+        outterSemaphore.put(2, westEntrance);
+        outterSemaphore.put(3, eastExit);
+        outterSemaphore.put(4, northEntrance);
+        outterSemaphore.put(5, southExit);
+        outterSemaphore.put(6, eastEntrance);
+        outterSemaphore.put(7, westExit);
     }
 }
